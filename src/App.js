b1d7b0de506c7389964870317b463c9174cc9a29
Bruno <bruno.cambianica@davidson.fr>
2018-11-02 11:50:57 +0100

    Sass

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+import './assets/stylesheets/style.css';
+
 import Footer from './partials/Footer'
 import Header from './partials/Header'
 import Layout from './partials/Layout'
 
 class App extends Component {
   render() {
-    let name = 'Bruno';
-    var getName = (firstName) => {
-      return firstName;
-    }
     return (
       <div className="App">
         <Header/>
--- src/partials/Layout.js
+++ src/partials/Layout.js
@@ -3,6 +3,8 @@ import { Route } from 'react-router-dom';
 import About from "../components/AboutComponent";
 import Home from '../components/HomeComponent';
 import Users from '../components/UserComponent';
+import '../assets/stylesheets/modules/about.css'
+
 class Layout extends Component {
     render() {
         return (
