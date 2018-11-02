38bd1e130d7a999afbbe3e4495ba9e6e68535851
Bruno <bruno.cambianica@davidson.fr>
2018-11-02 14:49:27 +0100

    User get api

--- src/components/UserComponent.js
+++ src/components/UserComponent.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
 class Users extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+            error: null
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:4000/clients')
+            .then(result => result.json())
+            .then(userList => {
+                console.log('liste ')
+                console.log(userList)
+                this.setState({ users: userList })
+            },
+                (err) => {
+                    this.setState({ error: err })
+                })
+    }
+
     render() {
         return (
             <div className="users-listing">
                 <h1>users list</h1>
+                <ul>
+                    {this.state.users.map((user) => (
+                        <li key={user._id}>
+                            {user.nom} {user.prenom}
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
